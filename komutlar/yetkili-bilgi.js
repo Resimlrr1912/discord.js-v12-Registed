4f03e8068a11f2cbfda9e0b47c6a398b954e45ac
Resimlrr1912 <lavbinmr@gmail.com>
2024-06-23 00:36:07 +0300

    Update yetkili-bilgi.js

--- komutlar/yetkili-bilgi.js
+++ komutlar/yetkili-bilgi.js
@@ -7,8 +7,8 @@ exports.run = async (client, message, args) => {
     var gün = [moment().format('DD-MM-YYYY | H:mm:ss')]
 let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 let tarih = doradb.fetch(`Yetkili.${member.id}`, gün) || "Henüz yetkili olmamış."
-if (!message.member.roles.cache.has("855901055992332308"))
-return message.channel.send(new Discord.MessageEmbed().setDescription(`❌ • Bu komutu kullanabilmek için <@&855901055992332308> olmalısın!`)).then(msg => msg.delete({timeout: 5000}));
+if (!message.member.roles.cache.has("1254173362880512010"))
+return message.channel.send(new Discord.MessageEmbed().setDescription(`❌ • Bu komutu kullanabilmek için <@&1254173362880512010> olmalısın!`)).then(msg => msg.delete({timeout: 5000}));
 if (!member) return message.channel.send(new Discord.MessageEmbed().setDescription('❌ • Kimin yetkili olduğu tarihi görmek istiyorsun o_O')).then(msg => msg.delete({timeout: 5000}));
 try{
 message.channel.send(new Discord.MessageEmbed().setTitle(`${member.user.tag}`) .addField("✅ • Yetkiye Başlama Tarihi", `${tarih}`).setFooter(message.author.tag, message.author.avatarURL({dynamic: true})) .setThumbnail(member.user.avatarURL({dynamic: true})).setColor('#D2EE07')).then(msg => msg.delete({timeout: 10000}));
