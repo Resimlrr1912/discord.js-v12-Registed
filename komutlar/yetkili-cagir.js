ce0182d7edab1218dede73f176ba149ac363a758
Resimlrr1912 <lavbinmr@gmail.com>
2024-06-22 23:20:11 +0300

    Update yetkili-cagir.js

--- komutlar/yetkili-cagir.js
+++ komutlar/yetkili-cagir.js
@@ -1,1 +1,17 @@
+const { MessageActionRow, MessageButton } = require('discord.js');
 
+module.exports = {
+    name: 'yetkili-cagir',
+    description: 'Yetkili çağırma butonu oluşturur.',
+    execute(message, args) {
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('yetkiliCagir')
+                    .setLabel('Yetkili Çağır')
+                    .setStyle('PRIMARY'),
+            );
+
+        message.channel.send({ content: 'Bir yetkili çağırmak için butona tıklayın:', components: [row] });
+    },
+};
